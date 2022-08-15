be00f9a2784d1faaa8d31ea9596e79a9df3ca5ed
Jakub Uryga <kub.uryga@gmail.com>
2022-08-15 17:15:15 +0200

    vinted clone development - initial (build) commit

---
+++ routers/main_board/router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, HTTPException
+from utils.main_board_utils import get_items
+
+router = APIRouter()
+
+
+@router.get("/items/", tags=["items"])
+async def retrieve_items():
+    return get_items()
+
+
+async def retrieve_item_by_id(item_id: str):
+    items = get_items()
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item Not Found")
+    return items[item_id]
---
+++ utils/main_board_utils.py
@@ -0,0 +1,5 @@
+def get_items():
+    return {
+        "1": {"item_name": "Blazer", "item_color": "Grey", "item_size": "L"},
+        "2": {"item_name": "Chino", "item_color": "Navy", "item_size": "XL"}
+    }
