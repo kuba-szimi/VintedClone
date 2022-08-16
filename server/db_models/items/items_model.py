9f838270f23426ebd37343d350530f01049187a0
Jakub Uryga <kub.uryga@gmail.com>
2022-08-17 01:06:03 +0200

    Updated db models for the users and items tables

--- server/db_models/items/items_model.py
+++ server/db_models/items/items_model.py
@@ -8,11 +8,11 @@ class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    brand = Column(String, index=True)
-    size = Column(String, index=True)
-    condition = Column(String, index=True)
-    color = Column(String, index=True)
+    title = Column(String(100), index=True)
+    brand = Column(String(50), index=True)
+    size = Column(String(10), index=True)
+    condition = Column(String(25), index=True)
+    color = Column(String(25), index=True)
     views = Column(Integer, index=True)
     upload_date = Column(DateTime)
     description = Column(String)
