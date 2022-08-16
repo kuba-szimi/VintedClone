d818473f0c79ddeaf8b8c1cfe576aea45acefdc6
Jakub Uryga <kub.uryga@gmail.com>
2022-08-17 00:43:05 +0200

    Corrected the directory and file structure of the project

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from server.routers import router as main_board_router
+from .routers.main_board.router import router as main_board_router
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
