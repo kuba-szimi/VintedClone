b818259127726e8048c1239aa7435b7eadeeac6f
Jakub Uryga <kub.uryga@gmail.com>
2022-08-17 01:06:27 +0200

    Updated the main file - server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from .routers.main_board.router import router as main_board_router
+from server.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
