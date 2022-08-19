b67e9b9b412989437b3c5dd4242217d8b653c839
Jakub Uryga <kub.uryga@gmail.com>
2022-08-19 02:26:54 +0200

    Updated server.py to resolve the issue with migration

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from .routers.main_board.router import router as main_board_router
 from server.database import Base, engine
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
