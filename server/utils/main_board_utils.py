440ada7bf260b7b20560483c6f50cba474b18cce
Jakub Uryga <kub.uryga@gmail.com>
2022-08-19 02:31:07 +0200

    Updated main board utils - retrieve_items and add_items functions

--- server/utils/main_board_utils.py
+++ server/utils/main_board_utils.py
@@ -1,3 +1,22 @@
+from server.db_models.users.users_model import User
+from server.db_models.items.items_model import Item
+from server.database import get_db
+from typing import List, Dict
+
+
+class ItemManager:
+
+    def __init__(self):
+        self._db = next(get_db())
+
+    def retrieve_items(self) -> List[Item]:
+        return self._db.query(Item).all()
+
+    def add_items(self, items_data: Dict):
+        #to be finished
+        self._db.insert()
+
+
 def get_items():
     return {
         "1": {"item_name": "Blazer", "item_color": "Grey", "item_size": "L"},
